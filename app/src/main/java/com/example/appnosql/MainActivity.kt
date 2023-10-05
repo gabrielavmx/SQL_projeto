1c92088776a32967f43c445cd530bc2d34b13d28
gabrielavmx <villegasmonterogabriela76@gmail.com>
2023-10-05 08:23:33 -0300

    Projeto sql

---
+++ app/src/main/java/com/example/appnosql/MainActivity.kt
@@ -0,0 +1,167 @@
+package com.example.appnosql
+
+import DBHandler
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appnosql.ui.theme.AppNoSqlTheme
+import com.example.appnosql.ui.theme.Pink80
+import com.example.appnosql.ui.theme.PurpleGrey40
+
+
+class MainActivity : ComponentActivity() {
+
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @RequiresApi(Build.VERSION_CODES.M)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppNoSqlTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(colors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
+                                title = {
+                                    Text(
+                                        text = "GFG",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center,
+                                        color = Color.White
+                                    )
+                                })
+                        }) {
+                        addDataToDatabase(LocalContext.current)
+                    }
+                }
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun addDataToDatabase(
+    context: Context
+) {
+
+    val activity = context as Activity
+    val courseName = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val courseDuration = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val courseTracks = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val courseDescription = remember {
+        mutableStateOf(TextFieldValue())
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+        var dbHandler: DBHandler = DBHandler(context)
+        Text(
+            text = "Cadastro de cartão",
+            color = PurpleGrey40, fontSize = 20.sp, fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            value = courseName.value,
+            onValueChange = { courseName.value = it },
+            placeholder = { Text(text = "Informe seu nome") },
+            modifier = Modifier
+                .fillMaxWidth(),
+            textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+            singleLine = true,
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            value = courseDuration.value,
+            onValueChange = { courseDuration.value = it },
+            placeholder = { Text(text = "Informe seu cvv") },
+            modifier = Modifier
+                .fillMaxWidth(),
+            textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+            singleLine = true,
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(
+            value = courseTracks.value,
+            onValueChange = { courseTracks.value = it },
+            placeholder = { Text(text = "Informe a data de validade") },
+            modifier = Modifier
+                .fillMaxWidth(),
+            textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+            singleLine = true,
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            value = courseDescription.value,
+            onValueChange = { courseDescription.value = it },
+            placeholder = { Text(text = "Numero do seu cartão") },
+            modifier = Modifier
+                .fillMaxWidth(),
+            textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+            singleLine = true,
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Button(
+            onClick = {
+            dbHandler.addNewCourse(
+                courseName.value.text,
+                courseDuration.value.text,
+                courseDescription.value.text,
+                courseTracks.value.text
+            )
+            Toast.makeText(context, "Course Added to Database", Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "Salvar dados", color = Color.White)
+        }
+    }
+}
